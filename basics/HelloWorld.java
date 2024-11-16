70fc2d9fd387cb2958931de2529182b6f7f93e43
Alexander Butenschön <139055703+dev667@users.noreply.github.com>
2024-11-16 19:53:40 +0100

    some formatting

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -1,14 +1,6 @@
-/*******************************************
- * Prints "Hello, World"
- * Everyone's first Java program.
- *******************************************/
-
 public class HelloWorld {
 
     public static void main(String[] args) {
-
         System.out.println("Hello, World");
-
     }
-
 }

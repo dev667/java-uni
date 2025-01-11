2ea180e30e7260b51ce815ddc5466223d40a1727
Alexander Butenschön <139055703+dev667@users.noreply.github.com>
2025-01-11 15:28:43 +0100

    formatting

--- Roman.java
+++ Roman.java
@@ -23,13 +23,13 @@ public class Roman {
     static String roman(int n) {
 
         // Declare values
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int[] figures =     {1000, 900, 500,  400, 100,  90,  50,  40,   10,   9,    5,   4,    1};
         String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
 
-        for (int i = 0; i < values.length; i++)
-            if (n >= values[i]) {
-                return numerals[i] + roman(n - values[i]);
+        for (int i = 0; i < figures.length; i++)
+            if (n >= figures[i]) {
+                return numerals[i] + roman(n - figures[i]);
             }
 
         // Base case
